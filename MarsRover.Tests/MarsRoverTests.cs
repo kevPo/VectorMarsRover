01705469269c0122c6109577b050980fbef8645e
Kevin Poston <kevposton@gmail.com>
2014-02-17 10:27:53 -0500

    Passed first test for MarsRover initialization

---
+++ MarsRover.Tests/MarsRoverTests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace MarsRover.Tests
+{
+    [TestFixture]
+    public class MarsRoverTests
+    {
+        [Test]
+        public void TestMarsRoverInitialization()
+        {
+            var marsRover = new MarsRover(100, "0,0", "N");
+        }
+    }
+}
---
+++ MarsRover/MarsRover.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MarsRover
+{
+    public class MarsRover
+    {
+
+        public MarsRover(Int32 planetSize, String roverStartingPoint, String direction)
+        {
+            
+        }
+    }
+}
